c7aeecd87f477ad40aae8f7a3605dd19d56e09d8
M0ller <simon.mollerberg@gmail.com>
2023-02-23 16:31:52 +0100

    uppdates

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
 import {loopMySqlTest} from "./mysql_methods.js";
 import {loopMongoTest} from "./mongodb_methods.js";
-import * as dotenv from 'dotenv'
-dotenv.config()
 
 await loopMySqlTest()
 // await loopMongoTest()

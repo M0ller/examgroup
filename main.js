3c9d013ddc925f0c17321789ec466607aff17182
M0ller <simon.mollerberg@gmail.com>
2023-02-28 14:52:10 +0100

    merged in mongodb

--- main.js
+++ main.js
@@ -1,10 +1,5 @@
-import {loopMySqlGetTest} from "./mysql/mysql-get.js";
-import {loopMongoGetTest} from "./mongodb/mongodb-get.js";
-import {
-    loopMySqlInsertTest
-} from "./mysql/mysql-insert.js";
-import {startMySqlConnection} from "./mysql/mysql-server.js";
-import {createMongodbCollection, dropMongodbCollection} from "./mongodb/mongodb-insert.js";
+
+import {loopMongodbInsertTest} from "./mongodb/mongodb-insert.js";
 import {closeMongoConnection, startMongoConnection} from "./mongodb/mongodb-server.js";
 
 console.log("**************************************************************")
@@ -18,9 +13,5 @@ console.log("MySQL INSERT test:")
 // await loopMySqlInsertTest()
 console.log("**************************************************************")
 console.log("MongoDB INSERT test:")
-// for (let i = 0; i < 10; i++) {
-    await createMongodbCollection()
-    await dropMongodbCollection()
-// }
-// await closeMongoConnection(con)
+await loopMongodbInsertTest()
 console.log("**************************************************************")
